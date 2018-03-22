f37a85f435451276cabc028be93db483ef7ff9ef
gabrielifeliz <34348458+gabrielifeliz@users.noreply.github.com>
2018-03-22 00:29:09 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/BullHorn02Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullHorn02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullHorn02Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Message;
+import com.example.demo.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @Autowired
+    MessageRepository messageRepository;
+
+    @RequestMapping("/")
+    public String listMessages(Model model) {
+        model.addAttribute("messages", messageRepository.findAll());
+        return "index";
+    }
+    @GetMapping("/add")
+    public String courseForm(Model model) {
+        model.addAttribute("message", new Message());
+        return "messageform";
+    }
+    @PostMapping("/process")
+    public String processForm(@Valid Message message, BindingResult result) {
+        if (result.hasErrors()) {
+            return "messageform";
+        }
+        messageRepository.save(message);
+        return "redirect:/";
+    }
+    @RequestMapping("/detail/{id}")
+    public String showCourse(@PathVariable("id") long id, Model model) {
+        model.addAttribute("message", messageRepository.findOne(id));
+        return "show";
+    }
+    @RequestMapping("/update/{id}")
+    public String updateCourse(@PathVariable("id") long id, Model model) {
+        model.addAttribute("message", messageRepository.findOne(id));
+        return "messageform";
+    }
+    @RequestMapping("/delete/{id}")
+    public String delCourse(@PathVariable("id") long id) {
+        messageRepository.delete(id);
+        return "redirect:/";
+    }
+}
